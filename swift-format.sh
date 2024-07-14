acdd6751c7aa03132cd1868db4e0df7de7f39d6f
Yasuhiro Hatta <hatta.yasuhiro@gmail.com>
2024-07-14 18:27:38 +0900

    Fix swift-format.sh

--- swift-format.sh
+++ swift-format.sh
@@ -3,12 +3,5 @@
 set -eu
 
 SCRIPT_DIT=$(cd "$(dirname "$0")"; pwd)
-cd "$SCRIPT_DIT"
-              
-swift format --in-place ./Package.swift
 
-swift format --in-place ./Sources/Hex/Data+Hex.swift
-swift format --in-place ./Sources/Hex/Sequence+Hex.swift
-
-swift format --in-place ./Sources/Hex/Data+HexTests.swift
-swift format --in-place ./Sources/Hex/Sequence+HexTests.swift
+swift format --in-place --parallel --recursive "$SCRIPT_DIT"
