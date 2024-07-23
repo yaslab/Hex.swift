07f29435c23005037f278568e1ce61285c12f398
Yasuhiro Hatta <hatta.yasuhiro@gmail.com>
2024-07-24 08:48:12 +0900

    Update benchmark.sh

--- benchmark.sh
+++ benchmark.sh
@@ -38,7 +38,7 @@ import Foundation
 import Hex
 
 let length = 5120
-let loop = 25000
+let loop = 25600
 
 let hexData = Data((0 ..< length).map({ _ in 0x30 }))
 let hexString = String(repeating: "0", count: length)
@@ -48,65 +48,78 @@ let bytes = Data(count: length)
 print("method | time (s)")
 print("--- | ---")
 
-do {
+@inline(never)
+func decodeData(_ n: Int) {
   let start = Date()
 
-  for _ in 0 ..< loop {
+  for _ in 0 ..< n {
     _ = Data(hexEncoded: hexData)!
   }
 
   print("\`Data(hexEncoded: data)\` | " + String(format: "%.4f", Date().timeIntervalSince(start)))
 }
 
-do {
+@inline(never)
+func decodeString(_ n: Int) {
   let start = Date()
 
-  for _ in 0 ..< loop {
+  for _ in 0 ..< n {
     _ = Data(hexEncoded: hexString)!
   }
 
   print("\`Data(hexEncoded: string)\` | " + String(format: "%.4f", Date().timeIntervalSince(start)))
 }
 
-do {
+@inline(never)
+func encodeCollectionIntoData(_ n: Int) {
   let start = Date()
 
-  for _ in 0 ..< loop {
+  for _ in 0 ..< n {
     _ = (bytes as any Collection<UInt8>).hexEncodedData()
   }
 
   print("\`Collection.hexEncodedData()\` | " + String(format: "%.4f", Date().timeIntervalSince(start)))
 }
 
-do {
+@inline(never)
+func encodeCollectionIntoString(_ n: Int) {
   let start = Date()
 
-  for _ in 0 ..< loop {
+  for _ in 0 ..< n {
     _ = (bytes as any Collection<UInt8>).hexEncodedString()
   }
 
   print("\`Collection.hexEncodedString()\` | " + String(format: "%.4f", Date().timeIntervalSince(start)))
 }
 
-do {
+@inline(never)
+func encodeSequenceIntoData(_ n: Int) {
   let start = Date()
 
-  for _ in 0 ..< loop {
+  for _ in 0 ..< n {
     _ = (bytes as any Sequence<UInt8>).hexEncodedData()
   }
 
   print("\`Sequence.hexEncodedData()\` | " + String(format: "%.4f", Date().timeIntervalSince(start)))
 }
 
-do {
+@inline(never)
+func encodeSequenceIntoString(_ n: Int) {
   let start = Date()
 
-  for _ in 0 ..< loop {
+  for _ in 0 ..< n {
     _ = (bytes as any Sequence<UInt8>).hexEncodedString()
   }
 
   print("\`Sequence.hexEncodedString()\` | " + String(format: "%.4f", Date().timeIntervalSince(start)))
 }
+
+decodeData(loop)
+decodeString(loop)
+encodeCollectionIntoData(loop)
+encodeCollectionIntoString(loop)
+encodeSequenceIntoData(loop)
+encodeSequenceIntoString(loop)
 EOF
 
 swift package clean
